e1c1b02dada115315544fabebbe47b3cc85f190d
안준우 <146637795+JunWoo0527@users.noreply.github.com>
2023-12-09 21:48:37 +0900

    [level 1] Title: 콜라 문제, Time: 0.18 ms, Memory: 65.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+        
+        while(n>=a) {
+            
+            answer += n/a*b;
+            n= n/a*b+n%a;
+            
+            System.out.println(n);
+            
+        }
+        
+        return answer;
+    }
+}
