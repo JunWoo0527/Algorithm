0e9349b1f121eeaa3aedf17223a278b8d9d72e84
안준우 <146637795+JunWoo0527@users.noreply.github.com>
2023-12-09 19:38:14 +0900

    [level 1] Title: K번째수, Time: 0.49 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -4,21 +4,13 @@ class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         
-//         String arr = Arrays.toString(array).replaceAll("[^0-9]","");
-        
-//         for(int a=0; a<commands.length; a++) {
-//             String subarr = arr.substring(commands[a][0]-1, commands[a][1]);
-            
-//             char[] carr = subarr.toCharArray();
-//             Arrays.sort(carr);
-//             char c = carr[commands[a][2]-1];
-            
-//             answer[a] =  Integer.valueOf(c-'0');
-//         }
         for(int a=0; a<commands.length; a++) {
             int[] subArray = Arrays.copyOfRange(array, commands[a][0]-1, commands[a][1]);
+            
             Arrays.sort(subArray);
+            
             answer[a] = subArray[commands[a][2]-1];
+            
             }
         return answer;
     }
