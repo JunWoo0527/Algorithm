eace9589a1b8f31b19d9dcd1ec45a55b57464fe5
안준우 <146637795+JunWoo0527@users.noreply.github.com>
2024-02-22 17:59:56 +0900

    [level 1] Title: 폰켓몬, Time: 3.86 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -5,28 +5,40 @@ class Solution {
         // 가져갈수있는 포켓몬의 수는 전체(N)/2
         // 모든 경우의 수를 조합해서 최대 다른종을 가져갈수있는 숫자는?
         
-        Arrays.sort(nums);
-        int getnum = nums.length / 2;
-        int sum = 0;
+//         Arrays.sort(nums);
+//         int getnum = nums.length / 2;
+//         int sum = 0;
         
-        for(int i=0; i<nums.length; i++) {
-            if(i == 0) {
-                sum++;
-                continue;
-            }
+//         for(int i=0; i<nums.length; i++) {
+//             if(i == 0) {
+//                 sum++;
+//                 continue;
+//             }
             
-            if(nums[i] == nums[i-1]) {
-                continue;
+//             if(nums[i] == nums[i-1]) {
+//                 continue;
                 
-            } else {
-                sum++;
-            }
-        }
-        if(sum >= getnum) {
-            sum = getnum;
+//             } else {
+//                 sum++;
+//             }
+//         }
+//         if(sum >= getnum) {
+//             sum = getnum;
+//         }
+        
+//         return sum;
+        
+        // HashSet 풀이
+        Set<Integer> intset = new HashSet<>();
+        
+        for(int i : nums) {
+            intset.add(i);
         }
         
-        return sum;
-
+        if( intset.size() > nums.length/2) {
+            return nums.length/2;
+        }
+        
+        return intset.size();
     }
 }
